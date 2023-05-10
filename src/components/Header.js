34b7e090b6e5b5d350f107b707a26036a9122d11
YiFan <jiao1995cn@gmail.com>
2023-05-10 03:46:50 -0500

    header created

---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+//rfce  
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      <div className='container grid header'>
+        <div className='page-title'>
+          MINI
+        </div>
+        <div className='head-nav grid'>
+          <div>
+            Home
+          </div>
+          <div>
+            Shop
+          </div>
+          <div>
+            Brand
+          </div>
+          <div>
+            About us
+          </div>
+        </div>
+        <div className='grid head-icons'>
+          <div className='head-icon'>
+            <img alt='' src={[require('../imgs/search.png')]}></img>
+          </div>
+          <div className='head-icon'>
+            <img alt='' src={[require('../imgs/buy.png')]}></img>
+          </div>
+          <div className='head-icon'>
+            <img alt='' src={[require('../imgs/contacts.png')]}></img>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
